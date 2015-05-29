83fb866c609011256a98ab5d8aad4528740babfd
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-29 09:25:39 -0400

    Add an illustrative example

--- examples/workflow.rs
+++ examples/workflow.rs
@@ -1,18 +1,3 @@
-# SQLite [![Build Status][status-img]][status-url]
-
-The package provides an interface to [SQLite][1].
-
-## [Documentation][doc]
-
-## Example
-
-The example given below can be ran using the following command:
-
-```
-cargo run --example workflow
-```
-
-```rust
 extern crate sqlite;
 
 use std::path::PathBuf;
@@ -41,16 +26,3 @@ fn setup() -> PathBuf {
     }
     path
 }
-```
-
-## Contributing
-
-1. Fork the project.
-2. Implement your idea.
-3. Open a pull request.
-
-[1]: https://www.sqlite.org
-
-[status-img]: https://travis-ci.org/stainless-steel/sqlite.svg?branch=master
-[status-url]: https://travis-ci.org/stainless-steel/sqlite
-[doc]: https://stainless-steel.github.io/sqlite
