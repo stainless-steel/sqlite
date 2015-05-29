6bece46d5b8ed9ca5942cb990a74011b73cb856c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-28 22:49:00 -0400

    Detail the test

--- tests/lib.rs
+++ tests/lib.rs
@@ -9,7 +9,11 @@ macro_rules! ok(
 );
 
 #[test]
-fn open() {
+fn execute() {
+    macro_rules! pair(
+        ($one:expr, $two:expr) => ((String::from($one), String::from($two)));
+    );
+
     let (path, _directory) = setup();
     let mut database = ok!(sqlite::open(&path));
 
@@ -19,10 +23,17 @@ fn open() {
     let sql = r#"INSERT INTO `users` (id, name, age) VALUES (1, "Alice", 20.99);"#;
     ok!(database.execute(sql, Some(|_| -> bool { true })));
 
+    let mut done = false;
     let sql = r#"SELECT * FROM `users`;"#;
-    ok!(database.execute(sql, Some(|_| -> bool {
+    ok!(database.execute(sql, Some(|pairs: Vec<(String, String)>| -> bool {
+        assert!(pairs.len() == 3);
+        assert!(pairs[0] == pair!("id", "1"));
+        assert!(pairs[1] == pair!("name", "Alice"));
+        assert!(pairs[2] == pair!("age", "20.99"));
+        done = true;
         true
     })));
+    assert!(done);
 }
 
 fn setup() -> (PathBuf, Directory) {
