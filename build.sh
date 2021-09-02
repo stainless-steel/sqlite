31145f447e8de73bd61de8e8180aa9e75fac9038
Alexey Proshutinskiy <alexey.prosh@fluence.one>
2021-09-02 18:58:44 +0300

    add tests, setup ci

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -o errexit -o nounset -o pipefail
 # set current working directory to script directory to run script from everywhere
 cd "$(dirname "$0")"
 
-# build aqua-dht.wasm
-marine build --release
+# build test.wasm
+marine build --release --bin test
 
 # copy .wasm to artifacts
 rm -f artifacts/*
@@ -14,6 +14,3 @@ cp target/wasm32-wasi/release/test.wasm artifacts/
 
 # download SQLite 3 to use in tests
 curl -L https://github.com/fluencelabs/sqlite/releases/download/v0.14.0_w/sqlite3.wasm -o artifacts/sqlite3.wasm
-
-# generate Aqua bindings
-#marine aqua artifacts/aqua-dht.wasm -s AquaDHT -i aqua-dht >../aqua/dht.aqua
