6f4e0c2d4f8c8da3253388b8fd25fe3e98aadc4b
vms <michail.vms@gmail.com>
2021-09-03 18:07:01 +0300

    use sqlite 15

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ mkdir -p artifacts
 cp target/wasm32-wasi/release/test.wasm artifacts/
 
 # download SQLite 3 to use in tests
-curl -L https://github.com/fluencelabs/sqlite/releases/download/v0.14.0_w/sqlite3.wasm -o artifacts/sqlite3.wasm
+curl -L https://github.com/fluencelabs/sqlite/releases/download/v0.15.0_w/sqlite3.wasm -o artifacts/sqlite3.wasm
