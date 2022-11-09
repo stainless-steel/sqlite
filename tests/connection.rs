f4c8c485a280006428db18458c605f93ccd3bf98
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-11-09 13:59:45 +0100

    Split the tests

---
+++ tests/connection.rs
@@ -0,0 +1,134 @@
+extern crate sqlite;
+extern crate temporary;
+
+use sqlite::{Connection, OpenFlags, State};
+
+mod common;
+
+use common::setup_users;
+
+macro_rules! ok(($result:expr) => ($result.unwrap()));
+
+#[test]
+fn change_count() {
+    let connection = setup_users(":memory:");
+    assert_eq!(connection.change_count(), 1);
+    assert_eq!(connection.total_change_count(), 1);
+
+    ok!(connection.execute("INSERT INTO users VALUES (2, 'Bob', NULL, NULL, NULL)"));
+    assert_eq!(connection.change_count(), 1);
+    assert_eq!(connection.total_change_count(), 2);
+
+    ok!(connection.execute("UPDATE users SET name = 'Bob' WHERE id = 1"));
+    assert_eq!(connection.change_count(), 1);
+    assert_eq!(connection.total_change_count(), 3);
+
+    ok!(connection.execute("DELETE FROM users"));
+    assert_eq!(connection.change_count(), 2);
+    assert_eq!(connection.total_change_count(), 5);
+}
+
+#[test]
+fn error() {
+    let connection = setup_users(":memory:");
+    match connection.execute(":)") {
+        Err(error) => assert_eq!(
+            error.message,
+            Some(String::from(r#"unrecognized token: ":""#))
+        ),
+        _ => unreachable!(),
+    }
+}
+
+#[test]
+fn iterate() {
+    macro_rules! pair(
+        ($one:expr, $two:expr) => (($one, Some($two)));
+    );
+
+    let connection = setup_users(":memory:");
+
+    let mut done = false;
+    let query = "SELECT * FROM users";
+    ok!(connection.iterate(query, |pairs| {
+        assert_eq!(pairs.len(), 5);
+        assert_eq!(pairs[0], pair!("id", "1"));
+        assert_eq!(pairs[1], pair!("name", "Alice"));
+        assert_eq!(pairs[2], pair!("age", "42.69"));
+        assert_eq!(pairs[3], pair!("photo", "\x42\x69"));
+        assert_eq!(pairs[4], ("email", None));
+        done = true;
+        true
+    }));
+    assert!(done);
+}
+
+#[test]
+fn open_with_flags() {
+    use temporary::Directory;
+
+    let directory = ok!(Directory::new("sqlite"));
+    let path = directory.path().join("database.sqlite3");
+    setup_users(&path);
+
+    let flags = OpenFlags::new().set_read_only();
+    let connection = ok!(Connection::open_with_flags(path, flags));
+    match connection.execute("INSERT INTO users VALUES (2, 'Bob', NULL, NULL)") {
+        Err(_) => {}
+        _ => unreachable!(),
+    }
+}
+
+#[test]
+fn open_with_full_mutex() {
+    use std::sync::Arc;
+    use std::thread;
+
+    let connection = ok!(Connection::open_with_full_mutex(":memory:"));
+    let connection = Arc::new(connection);
+
+    let mut threads = Vec::new();
+    for _ in 0..5 {
+        let connection_ = connection.clone();
+        let thread = thread::spawn(move || {
+            ok!(connection_.execute("SELECT 1"));
+        });
+        threads.push(thread);
+    }
+    for thread in threads {
+        ok!(thread.join());
+    }
+}
+
+#[test]
+fn set_busy_handler() {
+    use std::thread;
+    use temporary::Directory;
+
+    let directory = ok!(Directory::new("sqlite"));
+    let path = directory.path().join("database.sqlite3");
+    setup_users(&path);
+
+    let guards = (0..100)
+        .map(|_| {
+            let path = path.to_path_buf();
+            thread::spawn(move || {
+                let mut connection = ok!(sqlite::open(&path));
+                ok!(connection.set_busy_handler(|_| true));
+                let query = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
+                let mut statement = ok!(connection.prepare(query));
+                ok!(statement.bind((1, 2i64)));
+                ok!(statement.bind((2, "Bob")));
+                ok!(statement.bind((3, 69.42)));
+                ok!(statement.bind((4, &[0x69u8, 0x42u8][..])));
+                ok!(statement.bind((5, ())));
+                assert_eq!(ok!(statement.next()), State::Done);
+                true
+            })
+        })
+        .collect::<Vec<_>>();
+
+    for guard in guards {
+        assert!(ok!(guard.join()));
+    }
+}
---
+++ tests/cursor.rs
@@ -0,0 +1,115 @@
+extern crate sqlite;
+
+use sqlite::Value;
+use std::collections::HashMap;
+
+mod common;
+
+use common::{setup_english, setup_users};
+
+macro_rules! ok(($result:expr) => ($result.unwrap()));
+
+#[test]
+fn bind_by_name() {
+    let connection = ok!(sqlite::open(":memory:"));
+    ok!(connection.execute("CREATE TABLE users (id INTEGER, name STRING)"));
+    let statement = ok!(connection.prepare("INSERT INTO users VALUES (:id, :name)"));
+
+    let mut map = HashMap::<_, Value>::new();
+    map.insert(":name", "Bob".to_string().into());
+    map.insert(":id", 42.into());
+
+    let mut cursor = ok!(statement.into_cursor().bind_from(map));
+    assert!(cursor.next().is_none());
+}
+
+#[test]
+fn read() {
+    let connection = setup_users(":memory:");
+    ok!(connection.execute("INSERT INTO users VALUES (2, 'Bob', NULL, NULL, NULL)"));
+    let query = "SELECT id, age FROM users ORDER BY 1 DESC";
+    let statement = ok!(connection.prepare(query));
+
+    let mut count = 0;
+    for row in statement.into_cursor().map(|row| ok!(row)) {
+        let id = row.get::<i64, _>("id");
+        if id == 1 {
+            assert_eq!(row.get::<f64, _>("age"), 42.69);
+        } else if id == 2 {
+            assert_eq!(row.get::<Option<f64>, _>("age").unwrap_or(69.42), 69.42);
+        } else {
+            assert!(false);
+        }
+        count += 1;
+    }
+    assert_eq!(count, 2);
+}
+
+#[test]
+fn read_with_nullable() {
+    let connection = setup_users(":memory:");
+    let query = "SELECT id, name, email FROM users";
+    let statement = ok!(connection.prepare(query));
+
+    let row = ok!(ok!(statement.into_cursor().next()));
+    assert_eq!(row.get::<i64, _>("id"), 1);
+    assert_eq!(row.get::<Value, _>("id"), 1.into());
+    assert_eq!(row.get::<String, _>("name"), "Alice");
+    assert_eq!(row.get::<Value, _>("name"), String::from("Alice").into());
+    assert_eq!(row.get::<Option<String>, _>("email"), None);
+    assert_eq!(row.get::<Value, _>("email"), Value::Null);
+    assert_eq!(ok!(row.try_get::<Option<String>, _>("email")), None);
+}
+
+#[test]
+fn read_with_nullable_try_next() {
+    let connection = setup_users(":memory:");
+    let query = "SELECT id, name, email FROM users";
+    let statement = ok!(connection.prepare(query));
+
+    let mut cursor = statement.into_cursor();
+    let row = ok!(ok!(cursor.try_next()));
+    assert_eq!(ok!(row[0].as_integer()), 1);
+    assert_eq!(ok!(row[1].as_string()), "Alice");
+    assert_eq!(row[2].as_string(), None);
+}
+
+#[test]
+fn wildcard() {
+    let connection = setup_english(":memory:");
+    let query = "SELECT value FROM english WHERE value LIKE '%type'";
+    let statement = ok!(connection.prepare(query));
+
+    assert_eq!(statement.into_cursor().filter(|row| row.is_ok()).count(), 6);
+}
+
+#[test]
+fn workflow() {
+    let connection = setup_users(":memory:");
+
+    let select = "SELECT id, name FROM users WHERE id = ?";
+    let mut select = ok!(connection.prepare(select)).into_cursor();
+
+    let insert = "INSERT INTO users (id, name) VALUES (?, ?)";
+    let insert = ok!(connection.prepare(insert)).into_cursor();
+
+    for _ in 0..10 {
+        select = ok!(select.bind((1, 1)));
+        let row = ok!(ok!(select.next()));
+        assert_eq!(row.get::<i64, _>("id"), 1);
+        assert_eq!(row.get::<String, _>("name"), "Alice");
+        assert!(select.next().is_none());
+    }
+
+    let mut select = ok!(select.bind((1, 42)));
+    assert!(select.next().is_none());
+
+    let mut insert = ok!(insert.bind::<&[Value]>(&[42.into(), String::from("Bob").into()][..]));
+    assert!(insert.next().is_none());
+
+    let mut select = ok!(select.bind((1, 42)));
+    let row = ok!(ok!(select.next()));
+    assert_eq!(row.get::<i64, _>("id"), 42);
+    assert_eq!(row.get::<String, _>("name"), "Bob");
+    assert!(select.next().is_none());
+}
